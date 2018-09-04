5caaeb1e854176c2d2c3fe69175fea7c6f130fe4
kmdouglass <kyle.m.douglass@gmail.com>
2018-09-04 21:21:46 +0200

    Added new init.el

---
+++ init.el
@@ -0,0 +1,53 @@
+;;--------------------------------------------------------------------
+;; straight.el bootstrap
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;--------------------------------------------------------------------
+;; interactively do things
+(require 'ido)
+(ido-mode t)
+
+;;--------------------------------------------------------------------
+;; fill-column-indicator
+(straight-use-package 'fill-column-indicator)
+(require 'fill-column-indicator)
+(define-globalized-minor-mode
+  global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
+
+;;--------------------------------------------------------------------
+;; auto-complete mode
+(straight-use-package 'auto-complete)
+(ac-config-default)
+(global-auto-complete-mode t)
+
+;;--------------------------------------------------------------------
+;; flycheck
+(straight-use-package 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;;--------------------------------------------------------------------
+;; Custom
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tsdh-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
