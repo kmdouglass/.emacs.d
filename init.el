2bf2d9ec54ef5c48d0ed95e1be64f43bfc67ed8b
Kyle Douglass <kyle.douglass@pix4d.com>
2018-09-05 17:32:03 +0200

    Updates to flycheck for work

--- init.el
+++ init.el
@@ -25,7 +25,6 @@
 ;;--------------------------------------------------------------------
 ;; fill-column-indicator
 (straight-use-package 'fill-column-indicator)
-(require 'fill-column-indicator)
 (define-globalized-minor-mode
   global-fci-mode fci-mode (lambda () (fci-mode 1)))
 (global-fci-mode t)
