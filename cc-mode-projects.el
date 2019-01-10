c544d76b5a70aa7a9a5ab8ba7a48c67d935dc63a
Kyle Douglass <kyle.douglass@pix4d.com>
2019-01-10 11:24:36 +0100

    Removed project-specific cc-mode file

--- cc-mode-projects.el
+++
@@ -1,8 +0,0 @@
-(setq user-home (expand-file-name "~/"))
-
-(ede-cpp-root-project "RPi-DeviceAdapters"
-		      :file (concat (file-name-as-directory user-home) "src/RPi-DeviceAdapters/Makefile")
-		      :include-path '("src/DeviceAdapters/RPiGPIO"
-				      "src/DeviceAdapters/RPiTutorial"
-				      "src/DeviceAdapters/RPiV4L2")
-		      :system-include-path '("/opt/rpi-micromanager/micro-manager/MMDevice/"))
